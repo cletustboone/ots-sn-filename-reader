e6b201fae0ded8b176b0ad2cd771df35d3cb12c1
Clay Simmons <clay.simmons@gmail.com>
2013-11-04 09:00:14 -0500

    Removed chunk index from output. Added release support with grunt-release.

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function( grunt ) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    pkgVersion: "<%= pkg.version %>",
+
+    release: {
+      options: {
+        tagName: "v<%= version %>"
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-release");
+};
