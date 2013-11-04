565158bbc5cd9ceab6a6f9b2580aa53becb033c2
Clay Simmons <clay.simmons@gmail.com>
2013-11-04 09:02:19 -0500

    Don't prepend a v to version number. No functional changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,7 @@
 module.exports = function( grunt ) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-    pkgVersion: "<%= pkg.version %>",
-
-    release: {
-      options: {
-        tagName: "v<%= version %>"
-      }
-    }
+    pkgVersion: "<%= pkg.version %>"
   });
 
   grunt.loadNpmTasks("grunt-release");
